1acc59e4869a047b6e4b40b657075fb4ab81cd7e
Kevin <Kevindrake80@gmail.com>
2022-06-02 11:57:46 -0600

    Replaced the Files
    Added Incorrect Files, These are the correct ones

---
+++ TryDragon.java
@@ -0,0 +1,33 @@
+package genspark.assignments.section3;
+import java.util.Scanner;
+/*
+ * Went Back and added try Catch statement to where user inputs information.
+ */
+public class TryDragon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("You are in a land of dragons. In front of you there are two caves.\nIn one cave the dragon" +
+                "is friendly and will share his treasure.\nThe other dragon is greedy and hungry and will eat you on site. " +
+                "\nWhich cave will you go into? (1 or 2)");
+        int hungry =(int)( 1 + Math.random() * 3);
+        try {
+            int choice = input.nextInt();
+            if (choice == hungry) {
+                System.out.print("You approach the cave... \nIt is dark and spook...\n" +
+                        "A large dragon jumps out in front of you! He opens his jaws and..." +
+                        "\nGobbles you down in one bite!");
+            }
+            else {
+                System.out.print("You approach the cave, and hear a loud roar!\n" +
+                        "You continue to walk in and see mountains of glittering treasure!" +
+                        "\nThe dragon approaches you calmly and speaks to you in a low voice" +
+                        "\nI will share my hoard with you. Congratulations!");
+            }
+        }
+        catch (Exception ex) {
+            System.out.println("Caught Exception: Input. Please Retry");
+        }
+    }
+}
+
